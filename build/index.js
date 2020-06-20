68a218438d291902f76782198903459e2f662bc2
Arslaan Qadus <azy2k9@gmail.com>
2020-06-20 22:53:12 +0100

    A basic bubble sort that works for numbers only

--- build/index.js
+++ build/index.js
@@ -1,6 +1,22 @@
 "use strict";
-console.log('123');
-var logSomething = function () {
-    console.log('something');
-};
-logSomething();
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHandSide = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHandSide;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([3, 0, 10, -5, 55]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-console.log('123');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const logSomething = () => {
-  console.log('something');
-};
+  sort(): void {
+    const { length } = this.collection;
 
-logSomething();
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHandSide = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHandSide;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([3, 0, 10, -5, 55]);
+sorter.sort();
+console.log(sorter.collection);
